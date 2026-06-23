3091ae31cfd3f7da6bdba21cb4cb60de39f9aa78
khushiben <khushibenrana@gmail.com>
2026-06-23 10:41:49 +0530

    make some changes for betterment

--- server/server.js
+++ server/server.js
@@ -49,7 +49,7 @@ app.use("/api/notes", require("./routes/notes"));
 app.use("/api", require("./routes/progress"));
 app.use("/api/study-circle", require("./routes/studyCircle"));
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
